6285ac0d9b65a9d6120d4354d5fba5a6ed36a537
lazar0169 <lazar0169@gmail.com>
2021-06-05 03:37:33 +0200

    Game id in request changed

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -12,7 +12,7 @@ const getAllGames = async (_req, reply) => {
 
 const getSingleGame = async (req, reply) => {
     try {
-        const id = Number(req.params.id);
+        const id = req.params.id;
         let game = games.find((game) => game.id === id);
         if (game && !game.comments) {
             game.comments = await getCommentsFromUrl(game.commentsUrl);
